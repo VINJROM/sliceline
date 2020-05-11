a38cbc70288fc45e97fa49a5e9cd6ba5dacd0932
Vincent Romine <vinjrom@live.com>
2020-05-10 20:50:15 -0500

    update config.js

---
+++ build/precache-manifest.58ee7fb7554b3bd17fe9dfed424e83d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba74839c18a25e2d7fab4495e3b3738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc62e8d39f9c936c4f4a",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6bc4d614864b8306d31c",
+    "url": "/static/js/2.60897b72.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/static/js/2.60897b72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc62e8d39f9c936c4f4a",
+    "url": "/static/js/main.f8da3c3f.chunk.js"
+  },
+  {
+    "revision": "b0c9df8daf24241bca1f",
+    "url": "/static/js/runtime-main.f71725b2.js"
+  }
+]);
