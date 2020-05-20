ce375d0b71948dfc0c112531d89dd19873d1036a
Vincent Romine <vinjrom@live.com>
2020-05-19 23:49:11 -0500

    export functions.database

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,12 @@
-const functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers
+const functions = require("firebase-functions");
+
+// Pushes orders to function-database
+exports.sendUserEmail = functions.database
+  .ref("/orders/{pushId}")
+  .onCreate((order) => {
+    console.log("Sending User Email...");
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
