2c9b3a53d617bf2a0ff4c827b4ac9044c1b17b57
Vincent Romine <vinjrom@live.com>
2020-05-20 00:36:35 -0500

    update config.js

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,52 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers
 const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const htmlToText = require("nodemailer-html-to-text").htmlToText;
 
-// Pushes orders to function-database
+const { email, password } = require("./config");
+
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
+
+mailTransport.use("compile", htmlToText());
+
+const APP_NAME = "Sliceline";
+
 exports.sendUserEmail = functions.database
-  .ref("/orders/{pushId}")
+  .ref("/orders/{pushID}")
   .onCreate((order) => {
-    console.log("Sending User Email...");
+    sendOrderEmail(order.val());
   });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Pushes orders to function-database
+function sendOrderEmail(order) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@sliceline.com>`,
+    to: order.email,
+    subject: `Your order from ${APP_NAME}`,
+    html: `
+        <table style="width:500px; margin: auto">
+            <tr>
+                <th>${order.displayName}</th>
+                <th>You ordered some food, here's confirmation of that order.</th>
+            </tr>
+            ${order.order
+              .map(
+                ({ name, quantity, price }) => `
+                <tr>
+                    <td>${quantity}</td>
+                    <td>${name}</td>
+                    <td>${price}</td>
+                </tr>
+            `
+              )
+              .join("")}
+        </table>
+        `,
+  };
+  mailTransport.sendMail(mailOptions);
+}
