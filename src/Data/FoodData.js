b7f968a87c9b3cd1bd2db985a58b561485d74b81
Vincent Romine <vinjrom@live.com>
2020-06-04 13:18:50 -0500

    update menu

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,8 +1,8 @@
-export function formatPrice(price) {
-  return price.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD"
-  });
+export function formatPrice(price){
+  return price.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  })
 }
 
 export const foodItems = [
@@ -10,50 +10,87 @@ export const foodItems = [
     name: "Cheese Pizza",
     img: "/img/pizza.png",
     section: "Pizza",
-    price: 1
+    price: 5
   },
   {
     name: "Pepperoni Pizza",
     img: "/img/pizza2.jpeg",
     section: "Pizza",
-    price: 1.5
+    price: 5.5
   },
   {
     name: "Chicken Pizza",
     img: "/img/chicken-pizza.jpeg",
     section: "Pizza",
-    price: 2
-  },
-  {
-    img: "/img/healthy-pizza.jpeg",
-    name: "Veggie Pizza",
-    section: "Pizza",
-    price: 2
-  },
-  {
-    img: "/img/burger.jpeg",
-    name: "Burger",
-    section: "Sandwich",
-    price: 3
-  },
-  { img: "/img/gyro.jpeg", name: "Gyro", section: "Sandwich", price: 4.5 },
-  {
-    img: "/img/sandwich.jpeg",
-    name: "Shrimp PoBoy",
-    section: "Sandwich",
     price: 6
   },
   {
-    img: "/img/fries.jpeg",
-    name: "Fries",
-    section: "Sides",
-    price: 1
+    name: "Veggie Pizza",
+    img: "/img/healthy-pizza.jpeg",
+    section: "Pizza",
+    price: 6
   },
   {
-    price: 1,
-    name: "Soda",
-    section: "Drinks",
-    choices: ["Coke", "Sprite", "Root Beer"]
+    name: "Burger",
+    img: "/img/burger.jpeg",
+    section: "Sandwich",
+    price: 3
+  },
+  {
+    name: "Gyro",
+    img: "/img/gyro.jpeg",
+    section: "Sandwich",
+    price: 4
+  },
+  {
+    name: "Shrimp PoBoy",
+    img: "/img/sandwich.jpeg",
+    section: "Sandwich",
+    price: 4
+  },
+  {
+    name: "Melt",
+    img: "/img/melt.jpeg",
+    section: "Sandwich",
+    price: 4
+  },
+  {
+    name: "Fries",
+    img: "/img/fries.jpeg",
+    section: "Sides",
+    price: 2
+  },
+  {
+    name: "Garlic Sauce",
+    img: "/img/garlic-sauce.jpeg",
+    section: "Sides",
+    price: 0.5
+  },
+  {
+    name: "Ranch Sauce",
+    img: "/img/ranch-sauce.jpeg",
+    section: "Sides",
+    price: 0.5
+  },
+  {
+    name: "Marinara Sauce",
+    img: "/img/marinara-sauce.jpeg",
+    section: "Sides",
+    price: 0.5
+  },
+  {
+    price: 2.5,
+    name: 'Soda',
+    img: "/img/soda.jpeg",
+    section: 'Drinks',
+    choices: ['Coke', 'Coke Zero', 'Sprite', 'Root Beer', 'Fanta', 'Water']
+  },
+  {
+    price: 5,
+    name: 'Beer',
+    img: "/img/beer.jpeg",
+    section: 'Drinks',
+    choices: ['Budweiser', 'Bud Light', 'Coors Light', 'Micholob Golden Light', 'White Claw', 'Sierra Nevada IPA', 'Odouls N/A']
   }
 ];
 
